e22d1efc67088fa925aa5fd3291abd58e0685e77
vamshi <vamvijkum@gmail.com>
2019-10-26 14:41:03 +0530

    ChangeLog v2.3.6

--- lib/events/DumbCharades/dumb.dart
+++ lib/events/DumbCharades/dumb.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/material.dart';
 import 'dm.dart';
 
@@ -11,9 +13,10 @@ class _dumbState extends State<dumb> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("dumb"), toolbarOpacity: 1, backgroundColor: Color(0xff000000), centerTitle: true,),
+      appBar: AppBar(title: Text("Dumb Charades"), toolbarOpacity: 1, backgroundColor: Color(0xff000000), centerTitle: true,),
       body: Container(
-
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
         decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage("images/events.jpg"),
@@ -21,34 +24,31 @@ class _dumbState extends State<dumb> {
           ),
         ),
         child: Center(
-          child: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                width: MediaQuery.of(context).size.width / 1.05,
-                height: MediaQuery.of(context).size.height / 1.05,
-                child: Card(
-                  color: Color(0x99ffffff),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30.0)),
-                  child: Flex(direction: Axis.vertical, children: <Widget>[Expanded(
-                    child: new CustomScrollView(
-                      scrollDirection: Axis.vertical,
-                      shrinkWrap: false,
-                      slivers: <Widget>[
-                        new SliverPadding(
-                          padding: const EdgeInsets.symmetric(vertical: 0.0),
-                          sliver: new SliverList(
-                            delegate: new SliverChildBuilderDelegate(
-                                  (context, index) => new dm(),
-                              childCount: 1,
-                            ),
+          child: Padding(
+            padding: const EdgeInsets.only(top: 80.0),
+            child: Container(
+              width: MediaQuery.of(context).size.width / 1,
+              height: MediaQuery.of(context).size.height / 1,
+              child: Card(
+                color: Color(0xffffffff),
+                shape: BeveledRectangleBorder(borderRadius: BorderRadius.only(topLeft: Radius.circular(30.0),)),
+                child: Flex(direction: Axis.vertical, children: <Widget>[Expanded(
+                  child: new CustomScrollView(
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: false,
+                    slivers: <Widget>[
+                      new SliverPadding(
+                        padding: const EdgeInsets.symmetric(vertical: 0.0),
+                        sliver: new SliverList(
+                          delegate: new SliverChildBuilderDelegate(
+                                (context, index) => new dm(),
+                            childCount: 1,
                           ),
                         ),
-                      ],
-                    ),
-                  )],),
-                ),
+                      ),
+                    ],
+                  ),
+                )],),
               ),
             ),
           ),
