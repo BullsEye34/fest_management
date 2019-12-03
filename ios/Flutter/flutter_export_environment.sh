ab47010a219897d5e682c2d034be6d04d45e97ad
Vamshi Prasad <vamvijkum@gmail.com>
2019-12-03 23:20:58 +0530

    Clone to Linux

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=G:\SW\Flutter\SDK\flutter"
-export "FLUTTER_APPLICATION_PATH=F:\Android Projects\fest_management"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/home/vamshi/flutterSDK/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/vamshi/Desktop/Fest_Management"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=G:\SW\Flutter\SDK\flutter\bin\cache\artifacts\engine\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/home/vamshi/flutterSDK/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=2.4.2"
 export "FLUTTER_BUILD_NUMBER=1"
